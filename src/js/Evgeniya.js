44edb5abba287a5d3ed59e1913275957c3f1badd
evgeniabr <113677474+evgeniabr@users.noreply.github.com>
2023-04-08 16:35:25 +0300

    Update Evgeniya.js

---
+++ src/js/Evgeniya.js
@@ -0,0 +1,34 @@
+export default class Apiservice {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchMovieByQuery() {
+    console.log(this);
+    const BASE_URL = 'https://api.themoviedb.org/3/search/movie?';
+    const API_KEY = 'aa05d9ef1feff0e96ac321d8773a5c67';
+
+    const results = await fetch(
+      `${BASE_URL}?key=${API_KEY}&query=${this.searchQuery}&page=${this.page}`
+    );
+
+    return results;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    return (this.searchQuery = newQuery);
+  }
+
+  get page() {
+    return this.page;
+  }
+
+  set page(newPage) {
+    return (this.page = newPage);
+  }
+}
