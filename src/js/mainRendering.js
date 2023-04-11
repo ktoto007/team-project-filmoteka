b2416ff7169e8a277908194de2bfab3d5c80e915
InnaStarzhynska <113523884+InnaStarzhynska@users.noreply.github.com>
2023-04-11 19:13:40 +0300

    pagination
    Added pagination, changed export apiService

--- src/js/mainRendering.js
+++ src/js/mainRendering.js
@@ -1,24 +1,26 @@
 // Сюди бажано не лізти
-import NewFilmApiServise from './filmsAPIServise';
+import { apiService } from './filmsAPIServise';
 // import refs from './refs';
 import renderMarkupFilmsCard from './renderMarkupFilmsCard';
 import Loader from './loader';
+import buildingPagination from './pagination';
 
-const refs = {
+ export const refs = {
   homeHeaderForm: document.querySelector('.header__form'),
   cardContainer: document.querySelector('.cards-container'),
 };
 
-const apiService = new NewFilmApiServise();
 const loader = new Loader();
 
 document.addEventListener('DOMContentLoaded', receivingPopularFilms);
 
-async function receivingPopularFilms() {
+ export default async function receivingPopularFilms() {
   loader.show();
   const responce = await apiService.getPopularMovies();
   loader.hide();
 
+  apiService.requestType = 'trending';
+  buildingPagination(responce);
   return (refs.cardContainer.innerHTML = renderMarkupFilmsCard(
     responce.results
   ));
@@ -29,13 +31,15 @@ document.addEventListener('submit', gettingMoviesByKeyword);
 async function gettingMoviesByKeyword(e) {
   e.preventDefault();
 
-  apiService.type = 'search';
   apiService.query = refs.homeHeaderForm.query.value;
+  apiService.requestType = 'search';
+  console.log(apiService.requestType);
 
   loader.show();
   const responce = await apiService.fetchMovieByQuery();
   loader.hide();
 
+  buildingPagination(responce);
   return (refs.cardContainer.innerHTML = renderMarkupFilmsCard(
     responce.results
   ));
