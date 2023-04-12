b6d7297e5d8e77fc1e1e4b9e52a2ed42bcbef82b
Oleh Dashkevych <skoner@ukr.net>
2023-04-12 10:14:06 +0300

    Watched & Queue

--- src/js/WorkingFiles/Oleg.js
+++ src/js/WorkingFiles/Oleg.js
@@ -1,14 +1,54 @@
-export default class Loader {
-  constructor() {
-    this.loaderElement = document.createElement('div');
-    this.loaderElement.className = 'loader';
-  }
+// =================================== LOADER ===================================
 
-  show() {
-    document.body.appendChild(this.loaderElement);
-  }
+// export default class Loader {
+//   constructor() {
+//     this.loaderElement = document.createElement('div');
+//     this.loaderElement.className = 'loader';
+//   }
 
-  hide() {
-    this.loaderElement.remove();
+//   show() {
+//     document.body.appendChild(this.loaderElement);
+//   }
+
+//   hide() {
+//     this.loaderElement.remove();
+//   }
+// }
+
+
+// =================================== WATCHED & QUEUE ===================================
+
+
+function addToWatched(film) {
+  const currentMovie = film;
+
+  // перевірка на наявність даних в localStorage
+  const watchedMovies = JSON.parse(localStorage.getItem('watched')) || [];
+  
+  // перевірка на дублювання
+  if (!watchedMovies.find(watchedMovie => watchedMovie.id === currentMovie.id)) {
+
+  // додавання фільму до списку
+    watchedMovies.push(currentMovie);
+
+  // оновлення localStorage
+        localStorage.setItem('watched', JSON.stringify(watchedMovies));
+  }
+}
+
+function addToQueue(film) {
+  const currentMovie = film;
+  
+  // перевірка на наявність даних в localStorage
+    const queueMovies = JSON.parse(localStorage.getItem('queue')) || [];
+
+  // перевірка на дублювання
+  if (!queueMovies.find(queueMovie => queueMovie.id === currentMovie.id)) {
+      
+    // додавання фільму до списку
+    queueMovies.push(currentMovie);
+    
+    // оновлення localStorage
+        localStorage.setItem('queue', JSON.stringify(queueMovies));
   }
 }
