638c70484e087575d2ada85d1cd7e07c829c29be
Oleh Dashkevych <skoner@ukr.net>
2023-04-12 21:54:16 +0300

    Added function addToLocalStorage
    add to Watched and Queue

--- src/js/workWithLocalStorage.js
+++ src/js/workWithLocalStorage.js
@@ -13,7 +13,28 @@ async function getGenresInfo() {
 getGenresInfo();
 
 class LibraryLogic {
-  constructor() {}
+  constructor() { }
+  addToLocalStorage(film, listType) {
+  let moviesList;
+
+   // Перевірка типу списку та отримання відповідного списку фільмів з localStorage
+  if (listType === 'watched') {
+    moviesList = JSON.parse(localStorage.getItem('watched')) || [];
+  } else if (listType === 'queue') {
+    moviesList = JSON.parse(localStorage.getItem('queue')) || [];
+  }
+
+  // Перевірка на наявність дублікатів в списку фільмів
+  if (!moviesList.find(movie => movie.id === film.id)) {
+    
+    // Додавання фільму до списку
+    moviesList.push(film);
+    
+    // Оновлення localStorage
+    localStorage.setItem(listType, JSON.stringify(moviesList));
+  }
+}
+
   removeFromLocalstorage(film, type) {
     if (type == 'Queue') {
       const queueMovies = JSON.parse(localStorage.getItem('queue'));
@@ -27,13 +48,13 @@ class LibraryLogic {
       }
       return;
     } else {
-      const queueMovies = JSON.parse(localStorage.getItem('queue'));
+      const watchedMovies = JSON.parse(localStorage.getItem('watched'));
 
-      if (queueMovies.find(queueMovie => queueMovie.id === film.id)) {
+      if (watchedMovies.find(watchedMovie => watchedMovie.id === film.id)) {
         localStorage.setItem(
-          'queue',
+          'watched',
           JSON.stringify(
-            queueMovies.filter(queueMovie => queueMovie.id !== film.id)
+            watchedMovies.filter(watchedMovie => watchedMovie.id !== film.id)
           )
         );
       }
