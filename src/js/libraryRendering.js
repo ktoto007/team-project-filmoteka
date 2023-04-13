454431061fcaf668d513bb17987f439851dd3353
Streltsov Valerii <st.valera04@gmail.com>
2023-04-13 17:47:30 +0300

    working with library rendering

--- src/js/libraryRendering.js
+++ src/js/libraryRendering.js
@@ -3,15 +3,25 @@ import { refs } from './refs';
 import renderMarkupFilmsCard from './renderMarkupFilmsCard';
 document.addEventListener('DOMContentLoaded', gettingMoviesFromWatched);
 
-// refs.buttonWatched.addEventListener(
-//   'click',
-//   gettingMoviesFromWatced('watched')
-// );
+refs.buttonWatched.addEventListener('click', gettingMoviesFromWatched);
 
-// refs.buttonQueue.addEventListener('click', gettingMoviesFromWatced('queue'));
+refs.buttonQueue.addEventListener('click', gettingMoviesFromQueue);
 
 function gettingMoviesFromWatched() {
   const arr = libraryLogic.getFromStorage('watched');
+  refs.buttonWatched.classList.add('this-library');
+  refs.buttonQueue.classList.remove('this-library');
+  arr.map(item => {
+    const keys = item.genres.map(item => Object.values(item)[0]);
+    item.genre_ids = keys;
+  });
+  refs.cardContainer.innerHTML = renderMarkupFilmsCard(arr);
+}
+
+function gettingMoviesFromQueue() {
+  const arr = libraryLogic.getFromStorage('queue');
+  refs.buttonQueue.classList.add('this-library');
+  refs.buttonWatched.classList.remove('this-library');
   arr.map(item => {
     const keys = item.genres.map(item => Object.values(item)[0]);
     item.genre_ids = keys;
